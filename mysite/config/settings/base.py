ade4ad8c7b5fdbc37e26d4347a4e090a61e602e1
2siwon <lee89640245@gmail.com>
2017-11-24 01:02:50 +0900

    local, dev settings분류 및 settings/__init__.py 모듈 구성

--- mysite/config/settings/base.py
+++ mysite/config/settings/base.py
@@ -31,23 +31,8 @@ STATICFILES_DIRS = [
 STATIC_ROOT = os.path.join(ROOT_DIR, '.static_root')
 
 # Media paths
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = os.path.join(ROOT_DIR, '.media')
 MEDIA_URL = '/media/'
-
-# AWS
-AWS_ACCESS_KEY_ID = config_secret['aws']['aws_access_key_id']
-AWS_SECRET_ACCESS_KEY = config_secret['aws']['aws_secret_access_key']
-AWS_STORAGE_BUCKET_NAME = config_secret['aws']['s3_bucket_name']
-AWS_S3_SIGNATURE_VERSION = 's3v4'
-AWS_S3_REGION_NAME = 'ap-northeast-2'
-
-# AWS Storage
-STATICFILES_LOCATION = 'static'
-MEDIAFILES_LOCATION = 'media'
-
-# S3 FileStorage
-DEFAULT_FILE_STORAGE = 'config.storages.MediaStorage'
-STATICFILES_STORAGE = 'config.storages.StaticStorage'
 
 # Auth
 AUTH_USER_MODEL = 'member.User'
