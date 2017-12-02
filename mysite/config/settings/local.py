4f50c70e5bf4dd545363b2589afee89bef14b5ea
2siwon <lee89640245@gmail.com>
2017-12-02 23:04:25 +0900

    local db정보 settings_common_local로 이동

--- mysite/config/settings/local.py
+++ mysite/config/settings/local.py
@@ -10,12 +10,7 @@ ALLOWED_HOSTS = [
     '.siwon.me',
 ]
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+DATABASES = config_secret_common['django']['databases']
 
 SECRET_KEY = ''.join(
     [random.choice(string.ascii_lowercase) for i in range(40)])
